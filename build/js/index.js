a50c2dd51001ff923a6b36ad59081333541fe62e
web coders <webcoders1122@gmail.com>
2024-07-08 18:42:12 +0500

    initial configuration and code

---
+++ build/js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const newName = "name";
+console.log(newName);
+let a = 12;
+let b = "6";
+let c = 4;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const newName = "name";
+console.log(newName);
+
+let a = 12;
+let b = "6";
+let c = 4;
