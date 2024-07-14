e8cc358439a6e7ce2837840e8e93fd7c122be145
web coders <webcoders1122@gmail.com>
2024-07-14 14:51:39 +0500

    chapter-3: objects and enum is completed

--- Chapter-3/build/js/index.js
+++ Chapter-3/build/js/index.js
@@ -6,4 +6,27 @@ let strNumArr = ["str1", 12, "str2"]; // TS infers that it this array has union 
 let mixArr = ["str", 33, true]; // this is also a union type
 // tuple => this is strict array with fixed length and data
 let tuple = ["str", true, 22]; //this array has fixed length of 3 and web have to pass string on '0' index, boolean on '1' index and number on '3' index. and it is compulsory
-//Objects
+// Objects ---------------------------------------------------------------------
+let obj;
+// in this object, TS infers data types
+let object = {
+    name: "akram",
+    age: 23,
+    selected: true,
+};
+let object2 = {
+    name: "waqas",
+    age: 30,
+    selected: false,
+};
+// Enums ----------------------------------------------------------------------
+var sample;
+(function (sample) {
+    sample[sample["a"] = 100] = "a";
+    sample[sample["b"] = 101] = "b";
+    sample[sample["c"] = 102] = "c";
+    sample[sample["d"] = 103] = "d";
+    sample[sample["e"] = 104] = "e";
+    sample[sample["g"] = 105] = "g";
+})(sample || (sample = {}));
+console.log(sample.a);
--- Chapter-3/src/index.ts
+++ Chapter-3/src/index.ts
@@ -12,4 +12,45 @@ let mixArr = ["str", 33, true]; // this is also a union type
 
 let tuple: [string, boolean, number] = ["str", true, 22]; //this array has fixed length of 3 and web have to pass string on '0' index, boolean on '1' index and number on '3' index. and it is compulsory
 
-//Objects
+// Objects ---------------------------------------------------------------------
+let obj: object;
+
+// in this object, TS infers data types
+let object = {
+  name: "akram",
+  age: 23,
+  selected: true,
+};
+
+//how to assign a type to any object
+//both 'type' and 'interface' methods can be used but some differences are there. they will be explained in coming chapters
+type Student = {
+  name: string;
+  age?: number; //that '?' will make this key optional. we can not apply any javascript methods on optional properties without '?'. ie age?.ciel()
+  selected: boolean;
+};
+
+interface Student2 {
+  name: string;
+  age: number;
+  selected: boolean;
+}
+
+let object2: Student = {
+  name: "waqas",
+  age: 30,
+  selected: false,
+};
+
+// Enums ----------------------------------------------------------------------
+
+enum sample {
+  a = 100, // to provide default starting value else value will be '0'
+  b,
+  c,
+  d,
+  e,
+  g,
+}
+
+console.log(sample.a);
