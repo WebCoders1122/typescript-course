f940c03e2a217325d6fbf8c73c1d64af999fd629
web coders <webcoders1122@gmail.com>
2024-07-14 19:21:46 +0500

    chapter-3: functions, never type completed

--- Chapter-4/build/js/index.js
+++ Chapter-4/build/js/index.js
@@ -1,1 +1,27 @@
 "use strict";
+//but in interfaces, we can't do that.
+//literal types => special types like below
+let myName;
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(2, 3));
+// optional argument passed like this in any function
+const sumAll = (a, b, c) => {
+    if (typeof c !== "undefined")
+        return a + b + c;
+    return a + b;
+};
+//never type => happens when type can't be returned like below
+// const name: number & string = "never"; // uncomment this and this is never type
+const createError = (msg) => {
+    //bacause of this error, TS infers 'never' type for this function
+    throw new Error(msg);
+};
+const neverType = (value) => {
+    if (typeof value === "string")
+        return "String";
+    if (typeof value === "number")
+        return "Number";
+    return createError("false Value"); // this function has never type
+};
--- Chapter-4/src/index.ts
+++ Chapter-4/src/index.ts
@@ -14,3 +14,46 @@ type Student = {
   age: number;
   details: strOrNumArray; // this is type within type
 };
+//but in interfaces, we can't do that.
+
+//literal types => special types like below
+
+let myName: "abc" | "cde" | "xyz";
+// myName = 'sdfs' //this will not be assignable because we can assign only the above 3 values
+
+//================================================================================//
+
+//functions
+
+// type mathFunction = (a: number, b: number) => number;
+interface mathFunction {
+  // this is how to write interface for function
+  (a: number, b: number): number;
+}
+
+const add: mathFunction = (a, b) => {
+  return a + b;
+};
+
+console.log(add(2, 3));
+
+// optional argument passed like this in any function
+const sumAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") return a + b + c;
+  return a + b;
+};
+
+//never type => happens when type can't be returned like below
+
+// const name: number & string = "never"; // uncomment this and this is never type
+
+const createError = (msg: string) => {
+  //bacause of this error, TS infers 'never' type for this function
+  throw new Error(msg);
+};
+
+const neverType = (value: string | number): string => {
+  if (typeof value === "string") return "String";
+  if (typeof value === "number") return "Number";
+  return createError("false Value"); // this function has never type
+};
