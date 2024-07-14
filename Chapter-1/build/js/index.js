f794596f72295b410ed509cca2d749557747feff
web coders <webcoders1122@gmail.com>
2024-07-14 14:00:03 +0500

    chapter-1: some changes in tsconfig.json

---
+++ Chapter-1/build/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const newName = "name";
+console.log(newName);
+let a = 12;
+let b = 2;
+let c = 4;
+console.log(a / b);
---
+++ Chapter-1/src/index.ts
@@ -0,0 +1,8 @@
+const newName = "name";
+console.log(newName);
+
+let a = 12;
+let b: number = 2;
+let c = 4;
+
+console.log(a / b);
